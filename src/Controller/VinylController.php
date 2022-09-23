367455a5fc572e26325ba06bd64f53c8b1c84957
Yurii Fedoniuk <yura.nuke22@gmail.com>
2022-09-23 18:40:02 +0300

    2.15 it does not work, but for comparasion

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,10 +28,10 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(MixRepository $mixRepository, string $slug = null): Response
+    public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
+        $mixes = $this->getMixes();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
